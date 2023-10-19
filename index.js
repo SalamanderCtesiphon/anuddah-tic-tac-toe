ef0e17666000d7d051a3f0a550181d0afdbefeaa
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-19 14:29:47 -0500

    created the bare bone factory functions for the gameboard, player and game flow

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hi')
+// store the gameboard as an array inside of an object
+
+function gameBoard() {
+  const game_board = []
+  return { game_board }
+}
+
+// players are stored as objects as well
+
+function createUser(name) {
+  return { name }
+}
+
+// an object to control the flow of the game
+
+function gameFlow() {
+  return {}
+}
