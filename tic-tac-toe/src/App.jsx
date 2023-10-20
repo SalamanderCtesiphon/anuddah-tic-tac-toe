3ebf3fb792cac4445a5dc3baf57f6cc9942782a8
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-20 16:14:01 -0500

    created a basic layout for the game and styled it

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,7 +1,62 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  return <>hi</>
+  const [gameboard, setGameboard] = useState([
+    {
+      id: 1,
+      value: 'X',
+    },
+    {
+      id: 2,
+      value: 'O',
+    },
+    {
+      id: 3,
+      value: 'X',
+    },
+    {
+      id: 4,
+      value: 'X',
+    },
+    {
+      id: 5,
+      value: 'X',
+    },
+    {
+      id: 6,
+      value: 'O',
+    },
+    {
+      id: 7,
+      value: ' ',
+    },
+    {
+      id: 8,
+      value: ' ',
+    },
+    {
+      id: 9,
+      value: 'X',
+    },
+  ])
+  console.log(gameboard)
+  return (
+    <>
+      <div className="screen">
+        <h1>Tic Tac Toe Game</h1>
+        <div className="game-board">
+          {gameboard.map((square) => {
+            return (
+              <div key={square.id} className="game-square">
+                {square.value}
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App
