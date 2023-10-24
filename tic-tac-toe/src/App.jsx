f00788715ad80e1e21377a0d56f2f5489eaf4347
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-24 13:34:24 -0500

    created the game logic that allows the players to mark their square

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,31 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [gameboard, setGameboard] = useState([
     {
       id: 1,
-      value: 'X',
+      value: ' ',
     },
     {
       id: 2,
-      value: 'O',
+      value: ' ',
     },
     {
       id: 3,
-      value: 'X',
+      value: ' ',
     },
     {
       id: 4,
-      value: 'X',
+      value: ' ',
     },
     {
       id: 5,
-      value: 'X',
+      value: ' ',
     },
     {
       id: 6,
-      value: 'O',
+      value: ' ',
     },
     {
       id: 7,
@@ -37,19 +37,39 @@ function App() {
     },
     {
       id: 9,
-      value: 'X',
+      value: ' ',
     },
   ])
-  console.log(gameboard)
+
+  const [activePlayer, setActivePlayer] = useState('playerX')
+
+  function handleSquareClick(id) {
+    gameboard.map((square) => {
+      if (square.id === id) {
+        if (activePlayer === 'playerX') {
+          square.value = 'X'
+          setActivePlayer('playerO')
+        } else {
+          square.value = 'O'
+          setActivePlayer('playerX')
+        }
+      }
+    })
+    console.log(gameboard)
+    return gameboard
+  }
+
   return (
     <>
       <div className="screen">
-        <h1>Tic Tac Toe Game</h1>
+        <h2>Tic Tac Toe Game</h2>
         <div className="game-board">
           {gameboard.map((square) => {
             return (
               <div key={square.id} className="game-square cell">
-                <button>{square.value}</button>
+                <button onClick={() => handleSquareClick(square.id)}>
+                  {square.value}
+                </button>
               </div>
             )
           })}
